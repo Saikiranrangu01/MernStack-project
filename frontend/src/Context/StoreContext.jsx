c4b6629bba12845d6f2654113cc9cfd517fcf470
Rangu Saikiran <136926817+Saikiranrangu01@users.noreply.github.com>
2025-06-13 23:36:16 +0530

    backend url updated

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ import {toast} from 'react-hot-toast'
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://mernstack-project-backend-nacz.onrender.com";
   const [token,setToken] = useState("");
   const [food_list,setFoodList] = useState([]);
 
